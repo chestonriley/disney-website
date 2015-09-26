048f558aa8148b65ebc521406631a40280a4117a
fiestcheston <fiestcheston@yahoo.com>
2015-09-26 11:46:54 -0500

    Added the transportation page

--- php/menu.php
+++ php/menu.php
@@ -3,6 +3,7 @@
                 <li><a href="index.php">Home</li></a>
                 <li><a href="resort.php">Resort</li></a>
                 <li><a href="parkday.php">Park Day</li></a>
+                <li><a href="transportation.php">Transportation</li></a>
                 <li><a href="faqs.php">FAQs</li></a>
                 <li><a href="calendar.php">Calendar</li></a>
                 <li><a href="videos.php">Videos</li></a>
