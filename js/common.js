f36bf7ca83ea5a18a273b8c3dac84d1ca195330c
fiestcheston <fiestcheston@yahoo.com>
2015-09-26 19:21:30 -0500

    Changed transportation page to include light-boxes

--- js/common.js
+++ js/common.js
@@ -71,5 +71,107 @@ $("#home_menu").hover(function() {
 //    $("#home_menu").css("margin-right","-195px");
 //});
 
+//-- Transportation page light-box control
+var $light_box = $('<div id="lightbox"></div>');
+$('body').append($light_box);
+// Set up functions to return the html code for the lightbox
+
+// Rental Car lightbox
+function rental_car_lightbox() {
+    var lightbox = 
+        '<div class="trans_option_list" id="rental_car_list">' +
+                    '<h2 class="trans_list_header">Renting a Car</h2>' +
+                    '<div class="trans_option_list_left">' +
+                        '<h3 class="trans_list_header">Pros</h3>' +
+                        '<ul>' +
+                            '<li>Getting back and forth to some of the places that have easy parking is much better.  Downtown Disney, miniature golf courses, etc. that are not a part of the parks are a little easier and cheaper to get to by rental car.</li>' +
+                            '<li>In a medical “emergency”, getting to the clinic might be a little faster as well. </li>' +
+                            '<li>In addition, we nearly ALWAYS find ourselves making at least 2 trips to Walmart (just down the road) during our stay, and having a rental car makes it very convenient.</li>' +
+                        '</ul>' +
+                    '</div>' +
+                    '<div class="trans_option_list_right">' +
+                        '<h3 class="trans_list_header">Cons</h3>' +
+                        '<ul>' +
+                            '<li>The price.  You will pay about $400 for the week for a standard car.</li>' +
+                            '<li>You won’t be taking it to the parks anyway, since parking costs as well, and parking in the park lots is a nightmare, anyway. </li>' +
+                            '<li>Getting into a Disney resort (like the Boardwalk) is almost impossible in your own car.  They definitely frown on you parking in their GATED resort lots if you aren’t staying there, and even if you have a dinner reservation.</li>' +
+                        '</ul>' +
+                    '</div>' +
+        '</div>'
+        ;
+    return lightbox;
+}
+
+// Bus System lightbox
+function bus_system_lightbox() {
+            var lightbox = 
+            '<div class="trans_option_list" id="bus_system_list">' +
+                        '<h2 class="trans_list_header">Using the Bus System</h2>' +
+                        '<div class="trans_option_list_left">' +
+                            '<h3 class="trans_list_header">Pros</h3>' +
+                            '<ul>' +
+                                '<li>Once you get into the Disney bus system, it’s FREE.</li>' +
+                                '<li>The buses are well labeled, and you can get ANYWHERE on the Disney property. </li>' +
+                                '<li>It’s nice not to worry about where you put your car.</li>' +
+                            '</ul>' +
+                        '</div>' +
+                        '<div class="trans_option_list_right">' +
+                            '<h3 class="trans_list_header">Cons</h3>' +
+                            '<ul>' +
+                                '<li>It’s NOT free to take the Wyndham bus to the parks.  In 2014, the price was $5/person.  And there is NOT a Disney bus that comes to the resort (Wyndham is not Disney owned)</li>' +
+                                '<li>Figuring out how to get from place to place can be a bit confusing.</li>' +
+                                '<li>You get to ride with everyone else… for us, that’s a CON.</li>' +
+                            '</ul>' +
+                        '</div>' +
+                    '</div>';
+    return lightbox;
+}
+
+//Taxi lightbox
+function taxi_lightbox() {
+        var lightbox = 
+            '<div class="trans_option_list" id="taxi_list">' +
+                        '<h2 class="trans_list_header">Taking a Taxi*</h2>' +
+                        '<div class="trans_option_list_left">' +
+                            '<h3 class="trans_list_header">Pros</h3>' +
+                            '<ul>' +
+                                '<li>On-demand service.</li>' +
+                                '<li>The price isn’t too bad at roughly $20/trip </li>' +
+                                '<li>You are dropped off right at the entrance to the park.  That’s SUPER convenient if you’ve ever tried to park your car at one.</li>' +
+                                '<li>You can get into the Disney resorts (to do site-seeing/etc.) since you aren’t parking.</li>' +
+                            '</ul>' +
+                        '</div>' +
+                        '<div class="trans_option_list_right">' +
+                            '<h3 class="trans_list_header">Cons</h3>' +
+                            '<ul>' +
+                                '<li>$20/trip can add up, especially if you forget your tickets and have to go back to the resort (Been there, done that!)</li>' +
+                                '<li>You will need to plan ahead on each trip to ensure that you have enough time for the taxi to get there</li>' +
+                                '<li>The customer service isnt the best.  The dispatchers often have a hard time getting it right.  This can cause some extra wait at times.</li>' +
+                                '</ul>' +
+                            '</div>' +
+                        '<p style="padding: 20px; font-size: 1em;">* New from 2015:  The cab drivers HATE taking credit cards (I think its because they have to report it, then).  They definitely let you know it, too.  Just be sure to make it clear WHEN YOU GET IN THE CAR that you are paying with a credit card.</p>' +
+                    '</div>';
+    return lightbox;
+}
+
+$('#rental_car_thumbnail').click(function () {
+    var a = rental_car_lightbox();
+    console.log(a);
+    $('#lightbox').empty().append(a).fadeIn(800);
+});
+        
+$('#bus_system_thumbnail').click(function () {
+    var a = bus_system_lightbox();
+    $('#lightbox').empty().append(a).fadeIn(800);
+});
+
+$('#taxi_thumbnail').click(function () {
+    var a = taxi_lightbox();
+    $('#lightbox').empty().append(a).fadeIn(800);
+});
 
 
+//Hide the lightbox when it is clicked
+$light_box.click(function() {
+    $light_box.fadeOut(400);
+});
--- transportation.php
+++ transportation.php
@@ -40,69 +40,13 @@
             <div class="para-content">
                 <p>When planning for your Disney World adventure, you find that transportation is definitely one of the more expensive aspects of your magical vacation.  But the cold, hard fact is that the complex is far to big to walk anywhere (unless you are staying at the Boardwalk).  So considering what option to choose ahead of time can help you find the right balance between budget and "pain-in-the-neck" inconvenience.  It's just like anything else at Disney World:  You get what you pay for.</p>
                 <p>After staying there several years now, we've experienced all of the transportation options, and I've come up with some pros and cons for each of your transportation options.  It's important to note here that, most likely, your transportation decisions will NOT be made exclusive of other options.  Your transportation decisions will likely change day-to-day.</p>
-                <p>First, let's consider the option of getting a rental car.</p>
+                <p>Let's consider a list of potential options.  Click on each to see a list of pros and cons.</p>
                 
-                <div class="trans_option_list" id="rental_car_list">
-                    <h2 class="trans_list_header">Renting a Car</h2>
-                    <div class="trans_option_list_left">
-                        <h3 class="trans_list_header">Pros</h3>
-                        <ul>
-                            <li>Getting back and forth to some of the places that have easy parking is much better.  Downtown Disney, miniature golf courses, etc. that are not a part of the parks are a little easier and cheaper to get to by rental car.</li>
-                            <li>In a medical “emergency”, getting to the clinic might be a little faster as well. </li>
-                            <li>In addition, we nearly ALWAYS find ourselves making at least 2 trips to Walmart (just down the road) during our stay, and having a rental car makes it very convenient.</li>
-                        </ul>
-                    </div>
-                    <div class="trans_option_list_right">
-                        <h3 class="trans_list_header">Cons</h3>
-                        <ul>
-                            <li>The price.  You will pay about $400 for the week for a standard car.</li>
-                            <li>You won’t be taking it to the parks anyway, since parking costs as well, and parking in the park lots is a nightmare, anyway. </li>
-                            <li>Getting into a Disney resort (like the Boardwalk) is almost impossible in your own car.  They definitely frown on you parking in their GATED resort lots if you aren’t staying there, and even if you have a dinner reservation.</li>
-                        </ul>
-                    </div>
+                <div id="comparison_thumbnails_container">
+                    <button class="comparison_thumbnail" id="rental_car_thumbnail">Rental Car pros/cons</button>
+                    <button class="comparison_thumbnail" id="bus_system_thumbnail">Bus System pros/cons</button>
+                    <button class="comparison_thumbnail" id="taxi_thumbnail">Taxi pros/cons</button>                
                 </div>
-                <p>The next option to consider is via the bus system.  Disney has an enormous number of buses that run around the property all day long.  Sometimes I think that there are more buses than cars!:</p>
-                <div class="trans_option_list" id="bus_system_list">
-                    <h2 class="trans_list_header">Using the Bus System</h2>
-                    <div class="trans_option_list_left">
-                        <h3 class="trans_list_header">Pros</h3>
-                        <ul>
-                            <li>Once you get into the Disney bus system, it’s FREE.</li>
-                            <li>The buses are well labeled, and you can get ANYWHERE on the Disney property. </li>
-                            <li>It’s nice not to worry about where you put your car.</li>
-                        </ul>
-                    </div>
-                    <div class="trans_option_list_right">
-                        <h3 class="trans_list_header">Cons</h3>
-                        <ul>
-                            <li>It’s NOT free to take the Wyndham bus to the parks.  In 2014, the price was $5/person.  And there is NOT a Disney bus that comes to the resort (Wyndham is not Disney owned)</li>
-                            <li>Figuring out how to get from place to place can be a bit confusing.</li>
-                            <li>You get to ride with everyone else… for us, that’s a CON.</li>
-                        </ul>
-                    </div>
-                </div>
-                <p>Finally, you can take a taxi to just about anywhere you want.  We definitely recommend this option:</p>
-                <div class="trans_option_list" id="taxi_list">
-                    <h2 class="trans_list_header">Taking a Taxi*</h2>
-                    <div class="trans_option_list_left">
-                        <h3 class="trans_list_header">Pros</h3>
-                        <ul>
-                            <li>On-demand service.</li>
-                            <li>The price isn’t too bad at roughly $20/trip </li>
-                            <li>You are dropped off right at the entrance to the park.  That’s SUPER convenient if you’ve ever tried to park your car at one.</li>
-                            <li>You can get into the Disney resorts (to do site-seeing/etc.) since you aren’t parking.</li>
-                        </ul>
-                    </div>
-                    <div class="trans_option_list_right">
-                        <h3 class="trans_list_header">Cons</h3>
-                        <ul>
-                            <li>$20/trip can add up, especially if you forget your tickets and have to go back to the resort (Been there, done that!)</li>
-                            <li>You will need to plan ahead on each trip to ensure that you have enough time for the taxi to get there</li>
-                            <li>The customer service isn't the best.  The dispatchers often have a hard time getting it right.  This can cause some extra wait at times.</li>
-                            </ul>
-                        </div>
-                    <p style="padding: 20px; font-size: 1em;">* New from 2015:  The cab drivers HATE taking credit cards (I think it's because they have to report it, then).  They definitely let you know it, too.  Just be sure to make it clear WHEN YOU GET IN THE CAR that you are paying with a credit card.</p>
-                </div> 
                     
                 <p>Leaving the comfort of the resort to do some exploring can definitely be a bit stressful from the point of view of actually getting from place to place.  We find that the taxi option is the best balance of price vs. stress.  Last year's taxi driver experience was pretty bad for us, but at least you know what you are expecting, which is 90% of the battle.</p>
 
